52ba6d98c7be16aed00466442adf00577d6e7582
theweird0ne <heyamlan19@gmail.com>
2023-09-27 00:50:22 +0530

    feature: added users model & signup functionality

--- server/index.js
+++ server/index.js
@@ -7,12 +7,14 @@ app.use(express.json());
 
 //routes
 const productsRoutes=require('./routes/products.routes');
+const usersRoutes=require('./routes/users.routes')
 
 app.get('/',(req,res)=>{
     res.send("Its working")
 })
 
 app.use('/products',productsRoutes);
+app.use('/users',usersRoutes);
 
 app.listen(3000,()=>{
     console.log("Server running at port 3000");
---
+++ server/models/users.model.js
@@ -0,0 +1,34 @@
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true,
+    },
+    phoneNumber:{
+        type:Number,
+        required:true,
+        unique:true
+    }
+},{timestamps:true});
+
+const User = mongoose.model("User",userSchema);
+module.exports=User;
