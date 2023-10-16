27b778fc1e70fd7ad044e65032aafc2522a40e53
theweird0ne <heyamlan19@gmail.com>
2023-10-16 18:06:42 +0530

    feat: cart functionality completed

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -4,27 +4,52 @@ import { useEffect } from "react";
 import { useCartContext } from "../../contexts/cart.context";
 
 //actions
-import { fetchCart } from "../../actions/cart.action";
+import {
+  fetchCart,
+  removeFromCart,
+  updateCart,
+} from "../../actions/cart.action";
 
-export const Cart=()=>{
-    const {state,dispatch}=useCartContext();
+export const Cart = () => {
+  const { state, dispatch } = useCartContext();
 
-    const getCart=async()=>{
-        const cart=await fetchCart();
-        dispatch({type:"FETCH_CART",payload:cart})
-    }
+  const getCart = async () => {
+    const cart = await fetchCart();
+    dispatch({ type: "FETCH_CART", payload: cart });
+  };
 
-    useEffect(()=>{
-        // getCart();
-    },[]);
+  const removeCartItem = async (itemId) => {
+    await removeFromCart(itemId);
+    dispatch({ type: "REMOVE_FROM_CART", payload: itemId });
+  };
 
+  const handleQty = async (itemId, value) => {
+    await updateCart(itemId, value);
+    dispatch({
+      type: "UPDATE_QUANTITY",
+      payload: { id: itemId, qty: value },
+    });
+  };
 
-    return(
-        <section className="cart__section">
-            <h1>Cart page</h1>
-            {
-                state.cart && state.cart.length()
-            }
-        </section>
-    )
-}
+  useEffect(() => {
+    getCart();
+  }, []);
+
+  return (
+    <section className="cart__section">
+      <h1>Cart page</h1>
+      {state.cart &&
+        state.cart.map(({ _id, item, quantity }) => {
+          return (
+            <li key={_id}>
+              {item.name} ||{" "}
+              <button onClick={() => removeCartItem(_id)}>Remove</button>
+              <button onClick={() => handleQty(_id, quantity - 1)}>-</button> ||
+              Quantity:{quantity} ||{" "}
+              <button onClick={() => handleQty(_id, quantity + 1)}>+</button>
+            </li>
+          );
+        })}
+    </section>
+  );
+};
--- server/utils/cart.functions.js
+++ server/utils/cart.functions.js
@@ -1,56 +1,55 @@
-const User=require('../models/users.model');
+const User = require("../models/users.model");
 
-const addToCart=async(productId,userId)=>{
-    try{
-        const user=await User.findById(userId);
-        const cartItem={
-            item:productId
-        }
-        user.cart.push(cartItem);
-        await user.save();
-        return user.cart;
-    }catch(error){
-        console.error("Failed to add item to cart",error);
-    }
-}
+const addToCart = async (productId, userId) => {
+  try {
+    const user = await User.findById(userId);
+    const cartItem = {
+      item: productId,
+    };
+    user.cart.push(cartItem);
+    await user.save();
+    return user.cart;
+  } catch (error) {
+    console.error("Failed to add item to cart", error);
+  }
+};
 
-const getCartItems=async(userId)=>{
-    try{
-        const items=await User.findById(userId).populate('cart');
-        return items.cart;
-    }catch(error){
-        console.error("Failed to get cart items",error);
-    }
-}
+const getCartItems = async (userId) => {
+  try {
+    const items = await User.findById(userId).populate("cart.item");
+    return items.cart;
+  } catch (error) {
+    console.error("Failed to get cart items", error);
+  }
+};
 
-const updateQantity=async(userId,productId,qty)=>{
-    try{
-        const user=await User.findById(userId).populate('cart.item');
-        const cartItems=user.cart;
-        cartItems.map(cart=>{
-            if(cart.item._id.toHexString()===productId){
-                const newQty=cart.quantity+qty
-                cart.quantity=newQty;
-            }
-        })
-        await user.save();
-        return user.cart;
-    }catch(error){
-        console.error("Failed to update qunatity",error);
-    }
-}
+const updateQantity = async (userId, cartItemId, qty) => {
+  try {
+    const user = await User.findById(userId).populate("cart.item");
+    const cartItems = user.cart;
+    cartItems.map((item) => {
+      if (item._id.toHexString() === cartItemId) {
+        item.quantity = qty;
+      }
+    });
+    await user.save();
+    return user.cart;
+  } catch (error) {
+    console.error("Failed to update qunatity", error);
+  }
+};
 
-const deleteCartItem=async(productId,userId)=>{
-    try{
-        const user=await User.findById(userId).populate('cart.item');
-        const cartItems=user.cart;
-        const updatedCart=cartItems.filter(item=>item._id.toHexString()===productId);
-        user.cart=updatedCart;
-        await user.save();
-        return user.cart;
-    }catch(error){
-        console.error("Failed to delete cart item",error);
-    }
-}
+const deleteCartItem = async (cartItemId, userId) => {
+  try {
+    const user = await User.findById(userId).populate("cart.item");
+    const cartItems = user.cart;
+    const updatedCart = cartItems.filter(({ _id }) => _id === cartItemId);
+    user.cart = updatedCart;
+    await user.save();
+    return user.cart;
+  } catch (error) {
+    console.error("Failed to delete cart item", error);
+  }
+};
 
-module.exports={addToCart,getCartItems,updateQantity,deleteCartItem};
+module.exports = { addToCart, getCartItems, updateQantity, deleteCartItem };
