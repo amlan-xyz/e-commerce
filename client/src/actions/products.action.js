e848d3a29d6889f1d3df2719d61d98fc34334621
theweird0ne <heyamlan19@gmail.com>
2023-10-17 22:23:40 +0530

    update: added try catch to actions

--- client/src/actions/products.action.js
+++ client/src/actions/products.action.js
@@ -1,7 +1,15 @@
-const backend_api='http://localhost:3001';
+const backend_api = "http://localhost:3001";
 
-export const fetchProducts=async()=>{
-    const response=await fetch(`${backend_api}/products`);
-    const {data}=await response.json();
-    return data;
-}
+export const fetchProducts = async () => {
+  try {
+    const response = await fetch(`${backend_api}/products`);
+    const { data } = await response.json();
+    if (response.status === 200) {
+      return data;
+    } else if (response.status === 404) {
+      throw new Error("Cannot find products");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
