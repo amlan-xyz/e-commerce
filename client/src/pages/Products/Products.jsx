b7a32d61e7fdfd96e5e860f7d1d14b023af473a1
theweird0ne <heyamlan19@gmail.com>
2023-11-06 17:44:52 +0530

    update: redone products page

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
 //context
 import { useProductsContext } from "../../contexts/products.context";
 
@@ -10,8 +10,6 @@ import { fetchProducts } from "../../actions/products.action";
 import { Product } from "../../components/Product/Product";
 import { Sidebar } from "../../components/Sidebar/Sidebar";
 import "./Products.css";
-
-import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
 
 export const Products = () => {
   const { state, dispatch } = useProductsContext();
@@ -25,37 +23,94 @@ export const Products = () => {
     getProducts();
   }, []);
 
-  return (
-    <section className="products__section grid">
-      <div className="products__sidebar show__sidebar">
-        <Sidebar />
-      </div>
-      <div className="products__content">
-        <div className="products__header">
-          <h2>/ Our products </h2>
-          <div className="search__bar flex">
-            <AiOutlineSearch />
-            <input
-              type="text"
-              onChange={(e) =>
-                dispatch({ type: "SEARCH", payload: e.target.value })
-              }
-              value={state.searchQuery}
-              placeholder="Search....."
-            />
-          </div>
-          <button onClick={() => setShowFilter(true)} className="show__filter">
-            Filters
-          </button>
-        </div>
+  // return (
+  //   <section className="products__section">
+  //     <div className="products__sidebar show__sidebar">
+  //       <Sidebar />
+  //     </div>
+  //     <div className="products__content">
+  //       <div className="products__header">
+  //         <h2>/ Our products </h2>
+  //         <div className="search__bar flex">
+  //           <AiOutlineSearch />
+  //           <input
+  //             type="text"
+  //             onChange={(e) =>
+  //               dispatch({ type: "SEARCH", payload: e.target.value })
+  //             }
+  //             value={state.searchQuery}
+  //             placeholder="Search....."
+  //           />
+  //         </div>
+  //         <button onClick={() => setShowFilter(true)} className="show__filter">
+  //           Filters
+  //         </button>
+  //       </div>
 
-        <ul className="products__list grid">
-          {state.filteredProducts.map((product) => (
-            <li key={product._id} className="products__list-item">
-              <Product {...product} />
-            </li>
-          ))}
-        </ul>
+  //       <ul className="products__list grid">
+  //         {state.filteredProducts.map((product) => (
+  //           <li key={product._id} className="products__list-item">
+  //             <Product {...product} />
+  //           </li>
+  //         ))}
+  //       </ul>
+  //     </div>
+  //     {showFilter && (
+  //       <div className="modal">
+  //         <div className="modal_wrapper"></div>
+  //         <div className="modal_container">
+  //           <button
+  //             onClick={() => setShowFilter(false)}
+  //             className="modal_close-btn"
+  //           >
+  //             <AiOutlineClose />
+  //           </button>
+  //           <Sidebar />
+  //         </div>
+  //       </div>
+  //     )}
+  //   </section>
+  // );
+  return (
+    <section className="products__section">
+      <div className="products__sidebar " id="hide-sidebar">
+        <div className="products__sidebar-container">
+          <Sidebar />
+        </div>
+      </div>
+      <div className="products__body">
+        <div className="products__heading">
+          <h2>/ Our products </h2>
+          <div className="products__heading-body">
+            <div className="search__bar">
+              <AiOutlineSearch />
+              <input
+                type="text"
+                onChange={(e) =>
+                  dispatch({ type: "SEARCH", payload: e.target.value })
+                }
+                value={state.searchQuery}
+                placeholder="Search....."
+              />
+            </div>
+            <button
+              onClick={() => setShowFilter(true)}
+              id="show-sidebar"
+              className=" link__btn"
+            >
+              Filters
+            </button>
+          </div>
+        </div>
+        <div className="products__content">
+          <ul className="products__list grid">
+            {state.filteredProducts.map((product) => (
+              <li key={product._id} className="products__list-item">
+                <Product {...product} />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       {showFilter && (
         <div className="modal">
